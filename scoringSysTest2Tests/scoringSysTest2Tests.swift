5fa2506664853db28dcfa4a46942a1f1f699c2a0
ForrestLi <forrestliyx@gmail.com>
2020-07-18 13:36:36 +0800

    Initial Commit

---
+++ scoringSysTest2Tests/scoringSysTest2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  scoringSysTest2Tests.swift
+//  scoringSysTest2Tests
+//
+//  Created by Forrest Li on 2020/7/18.
+//  Copyright © 2020 Forrest Li. All rights reserved.
+//
+
+import XCTest
+@testable import scoringSysTest2
+
+class scoringSysTest2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
